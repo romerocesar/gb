f46c49b25a4c94997bb4f4a13aa3858cb56ad7bf
cesar <cesar@romero.ws>
2013-07-26 09:19:38 -0700

    Adds bootstraping option to the mongo DAL constructor. Updates menu template to be more jquery mobile friendly which will help us down the road.

--- orders/dal/mongo.py
+++ orders/dal/mongo.py
@@ -1,11 +1,18 @@
+import os
 import pymongo
 
 class OrdersDAO:
     '''This class defines the data access object to be used for data
     stored in MongoDB'''
     
-    def __init__(self):
+    def __init__(self, bootstrap=False):
         self.client = pymongo.MongoClient()
+        if bootstrap:                        # load data from dir
+            self.client.orders.menus.drop()
+            self.client.orders.menus.insert({'Food':['Appetizers', 'Main Dishes', 'Desserts'],
+                                        'Drinks':['Sodas','Juices', 'Wines', 'Beers', 'Cocktails']})
+            self.client.orders.dishes.drop()
+            self.client.orders.dishes.insert({'name':'T-Bone', 'description':'Awesome meat!', 'price':15.99})
 
     def get_menu(self,client_id=1):
         '''for now just get the first menu in the collection and
--- orders/settings.py
+++ orders/settings.py
@@ -1,11 +1,14 @@
 # import the data access layer (DAL)
 from dal import mongo, test
 
-# DB access config. This data access object (DAO) will be used 
-# for all DB access in the orders app. This allows us to transparently change
+# DB access config. This data access object (DAO) will be used for all
+# DB access in the orders app. This allows us to transparently change
 # the DB from Mongo to Postgres or anything else by simply changing
 # this line and not changing anything in the application layer. For
 # example, enable the test DAO by simply uncommenting the following
-# line:
+# line. Also, you have the option of bootstraping the DB by cleaning
+# it up and loading it with test data by simply passing True to the
+# constructor. See dal/mongo.py to see the data being loaded. NOTE:
+# only enable this in a dev/test environment - never in production
 # dao = test.OrdersDAO()
 dao = mongo.OrdersDAO()
